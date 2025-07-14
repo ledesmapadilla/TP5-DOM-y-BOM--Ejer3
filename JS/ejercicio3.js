1e43601543ec183890fe75c568a1b8bdb0cf93d5
ignaciolp <ignaciolp@lepa.com.ar>
2025-07-14 19:32:30 -0300

    agrego la parte de eliminar tarjetas

--- JS/ejercicio3.js
+++ JS/ejercicio3.js
@@ -6,21 +6,36 @@ const cargarTarea = (e) => {
   const tarea = formulario[0].value;
   console.log(tarea);
 
+  /* crear card */
   const card = document.createElement("div");
   card.className = "col-md-4 col-lg-3";
-  card.innerHTML = `<div class="card"><div class="card-body text-center"><h5 class="card-title">Tarea</h5><p class="card-text">${tarea}</p></div></div>`;
+  card.innerHTML = `<div class="card"><div class="card-body text-center"><h5 class="card-title">Tarea</h5><p class="card-text">${tarea}</p><button class="btn btn-primary">Eleminar</button></div></div>`;
+
+
+  /* borrar card (esto va inmediatamente despues de crear la card) */
+  const tareaBorrada = card.querySelector(".btn-primary");
+  console.log(tareaBorrada);
+
+  tareaBorrada.addEventListener("click", () => {
+    card.remove();
+  });
+
   nuevaCard.appendChild(card);
 
   formulario.reset();
+
+  /* const tareaFuera = tareaBorrada.addEventListener("click", () => {
+    nuevaCard.remove();
+    console.log(tareaFuera);
+  }); */
 };
+
+/* borrar tarea */
 
 const formulario = document.querySelector(".w-50");
 console.log(formulario);
 
 const nuevaCard = document.querySelector(".row");
 console.log(nuevaCard);
-
-/* const tareaCard = document.querySelector(".card-text");
-console.log(tareaCard); */
 
 formulario.addEventListener("submit", cargarTarea);
